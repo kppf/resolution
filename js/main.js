58dfd479d8d73d11b82a076bfe05a8a13c1354f4
Kushagra Pandey <kushagra4296@gmail.com>
2013-05-14 23:37:59 +0530

    radio buttons working. changing the resolution

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
+var resolutions = [ [1360,768],[1280,960],
+                    [1280,800],[1280,720],
+                    [1152,864],[1024,780],
+                    [800 ,600],[640 ,480]
+                   ];
+
 $(document).ready(function(){
-    $("#chk1").change(function(){
-        if(this.checked)
-            $("#frame").attr("src", $('#url').val());
+    $(":radio").change(function(){
+        if($(this).is(':checked')){
+            $index = $('input[name=radio]:checked').index('input[name=radio]');
+            console.log($index);
+            
+            $('#frame').css({'width':resolutions[$index][0], 'height':resolutions[$index][1]});
+        }
     });
     
     $('#urlform').submit(function(e){
         $("#frame").attr("src", geturl());
+        $('#url').val(geturl());
     });
 });
 
